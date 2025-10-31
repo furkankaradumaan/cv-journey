38213f2e2ec92981785ed8af0d7aabb10754c88c
Furkan Karaduman <furkaankaraduman@gmail.com>
2025-10-31 23:37:35 +0300

    Added repo.py and test_hithub_repo_object.py
    I added a repo.py class to represent a Github repository. I also wrote tests for this class in test/test_github_repo_object.py file.

---
+++ week01/Github_Trendings_Scraper/repo.py
@@ -0,0 +1,76 @@
+"""
+This file defines an article class to represent
+a Github article.
+"""
+
+from dataclasses import dataclass, field
+from functools import wraps
+import types
+
+class InvalidAttributeError(Exception):
+    """
+    This expection raised when a GithubRepo object
+    is tried to instantiate with invalid parameters.
+    """
+    pass
+
+@dataclass(slots=True)
+class GithubRepo:
+    """
+    This class represents information about a Github repository.
+    """
+    owner: str = field(default=None) # Username of the owner
+    repo_name: str = field(default=None) # Repository name
+    description: str = field(default="") # The description line of Repo
+    language: str = field(default="") # Mostly used programming language
+    stars: int = field(default=None) # Number of stars the repository have.
+    shares: int = field(default=None)
+    
+    def __post_init__(self):
+        """
+        Checks the instance variables of the objet and validates them.
+        """
+        # Firstly, we need to loop over the instance variables
+        for attribute_name in self.__slots__:
+            attribute_value = self.__getattribute__(attribute_name) # Get the value of the attribute
+            attribute_type = type(attribute_value) # Find the attribute type
+            expected_type = self.__annotations__.get(attribute_name, None) # Get the expected type for attribute.
+                                                                          # None if expected type is not specified.
+
+            if isinstance(expected_type, types.UnionType): # If the type is UnionType, it means there is
+                                                           # more than one possible types for the attribute.
+                if attribute_type not in expected_type.__args__: # Search the expected types in all possible types.
+                    raise InvalidAttributeError(f"{attribute_name} is type of {attribute_type.__name__}."
+                                                 f"Expected type is one of {expected_type}.")
+            elif isinstance(expected_type, type): # If the expected_type object is just a single type
+                                                  # compare the type with the type of attribute.
+                if attribute_type != expected_type:
+                    raise InvalidAttributeError(f"{attribute_name} is type of {attribute_type.__name__}."
+                                                f"Expected type is {expected_type}.")
+
+        # Now we will check the lengths of the inputs.
+        # The string type objects have to be at least 1 character except blanks.
+        if len(self.owner.strip()) == 0:
+            raise InvalidAttributeError("Owner name must be at least one character long.")
+        if len(self.repo_name.strip()) == 0:
+            raise InvalidAttributeError("Repository name must be at least one character long.")
+        
+        #  Validate stars and shares.
+        if self.stars < 0:
+            raise InvalidAttributeError("Stars cannot be negative.")
+        if self.shares < 0:
+            raise InvalidAttributeError("Shares cannot be negative.")
+
+    def to_dict(self):
+        """
+        Writes the object into a dictionary and returns it.
+        """
+        dict_ = dict()
+        
+        # Loop through the instance attributes in specified in __slots__ variable.
+        for attribute_name in self.__slots__:
+            attribute_value = self.__getattribute__(attribute_name)
+            assert attribute_value is not None, "Attribute {attribute_name} is None" # Attribute value cannot be None.
+            dict_[attribute_name] = attribute_value # Put the key-value pair into dict.
+
+        return dict_
---
+++ week01/Github_Trendings_Scraper/test/test_github_repo_object.py
@@ -0,0 +1,101 @@
+"""
+This files purpose is to test the GithubRepo dataclass in
+the file repo.py.
+"""
+
+import sys, os
+# We will add the path of the parent directory, so
+# we can include repo.py file.
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from repo import GithubRepo, InvalidAttributeError
+
+import pytest # Test framework
+
+
+@pytest.mark.parametrize("owner, repo_name, description, language, stars, shares",
+                         [
+                            ("furkan", "repo", "repo desc", "Python", 11, 10),
+                            ("fatih", "repo2", "description", "Java", 0, 1),
+                            ("yasir", "notebook", "nodesc", "C++", 0, 0),
+                            ("talha", "homework", "", "", 1, 0),
+                            ("gürkan", "atm-machine", "useful ATM", "C", 100, 20),
+                            ("betül", "barbie-home", "I love barbie", "", 18, 7),
+                            ("username", "a", "desc", "Ruby", 0, 0),
+                             ])
+def test_repo_creation_successfull(owner, repo_name, description,
+                                   language, stars, shares):
+    """
+    This function checks the creation process with valid arguments.
+    We expect that all the cases the object is created successfully and
+    the values of the attributes of the instance variable are matches.
+    """
+    repo = GithubRepo(owner, repo_name, description, language, stars, shares)
+    
+    assert repo.owner == owner, "Owner name is wrong"
+    assert repo.repo_name == repo_name, "Repo name is wrong"
+    assert repo.description == description, "Description is wrong"
+    assert repo.language == language, "Language is wrong"
+    assert repo.stars == stars, "Stars is wrong"
+    assert repo.shares == shares, "Shares is wrong"
+
+
+@pytest.mark.parametrize("owner, repo_name, description, language, stars, shares",
+                         [
+                            ("", "repo_name", "repo_desc", "Python", 10, 11), # Username has a length of 0, invalid.
+                            (None, "firstrepo", "first project", "Java", 1, 5), # Username is None, invalid.
+                            ("furkan", "", "my project", "Rust", 3, 6), # Repository name has a length of 0, invalid.
+                            ("fatih", None, "description", "TypeScript", 5, 9), # Repository name is None, invalid.
+                            ("emir", "ATM", None, "C#", 1, 0), # Description is None, invalid.
+                            ("betül", "barbie", "description", None, 4, 6), # Language is None, invalid.
+                            ("eren", "cproject", "desc", "C++", -2, 4), # Stars negative, invalid.
+                            ("tarık", "atm", "desc", "Java", 4, -1), # Shares negative, invalid.
+                            (120, "atm", "desc", "Java", 12, 12), # Username is not a string, invalid.
+                            ("furkan", True, "desc", "Python", 0, 1), # Repo name is not a string, invalid.
+                            ("deniz", "atm", 10, "C#", 4, 1), # Description is not a string, invalid.
+                            ("korhan", "pswman", "desc", dict(), 0, 0), # Language is not a string, invalid.
+                            ("furkan", "webapp", "flask", "Python", 120.40, 20), # Stars is not int, invalid.
+                            ("linus", "kernel", "UNIX-based", "C", 20, 10.33), # Shares is not int, invalid.
+                             ])
+def test_repo_creation_failed(owner, repo_name, description,
+                              language, stars, shares):
+    """
+    This function checks the creation process with invalid arguments.
+    In all cases we should get an error.
+    """
+    with pytest.raises(InvalidAttributeError):
+        repo = GithubRepo(owner, repo_name, description, language, stars, shares)
+
+
+@pytest.mark.parametrize("owner, repo_name, description, language, stars, shares",
+                         [
+                            ("furkan", "repo", "desc", "Python", 0, 0),
+                            ("betül", "barbie", "love barbie", "", 1, 1),
+                            ("talha", "yeni-şarkı", "spotify'da", "", 100, 120),
+                            ("gürkan", "file-manager", "C file manager", "C", 200, 1000),
+                             ])
+def test_to_dictionary(owner, repo_name, description,
+                        language, stars, shares):
+    
+    attr_names = [
+                    "owner",
+                    "repo_name",
+                    "description",
+                    "language",
+                    "stars",
+                    "shares",
+                  ]
+    values = [
+                owner,
+                repo_name,
+                description,
+                language,
+                stars,
+                shares,
+                ]
+
+    expected = {key:value for key, value in zip(attr_names, values)}
+
+    repo = GithubRepo(*values)
+    dict_ = repo.to_dict()
+
+    assert dict_ == expected
