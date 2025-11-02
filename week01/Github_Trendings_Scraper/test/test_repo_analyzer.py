cdeda6a5c9e8fc899f7fb44a7fe56658d7f087fb
Furkan Karaduman <furkaankaraduman@gmail.com>
2025-11-02 14:58:53 +0300

    Modified repo_analyzer.py and test_repo_analyzer.py
    I completed all the tests for RepoAnalyzer class

--- week01/Github_Trendings_Scraper/test/test_repo_analyzer.py
+++ week01/Github_Trendings_Scraper/test/test_repo_analyzer.py
@@ -92,41 +92,57 @@ def analyzer(sample_repos):
 
     
 class TestRepoAnalyzerManager:
-    def test_apply_suitable_test(self, analyzer, test_cases):
+    def test_repo_analyzer(self, analyzer, test_cases):
         '''
-        This method loops through each test_case.
-        Determines which method to call for each test
-        case and calls that test method.
+        This method loops through all the cases and
+        applies the test according to given test_method
         '''
         for test_key, test_case in test_cases.items():
+            test_input = test_case['input']
+            
             test_method = test_case['test_method']
             if test_method == 'minimum_stars':
-                self._minimum_stars_test(analyzer, test_case)
+               result = analyzer.minimum_stars(test_input).get()
             elif test_method == 'maximum_stars':
-                pass
-                #self._maximum_stars_test(analyzer, test_case) # APPLY
+                result = analyzer.maximum_stars(test_input).get()
             elif test_method == 'most_starred_n':
-                pass
-                #self._most_starred_n_test(analyzer, test_case) ## APPLY
+                result = analyzer.most_starred_n(test_input).get()
             elif test_method == 'minimum_forks':
-                pass
-                #self._minimum_forks_test(analyzer, test_case) # APPLy
+                result = analyzer.minimum_forks(test_input).get()
             elif test_method == 'maximum_forks':
-                pass
-                #self._maximum_forks_test(analyzer, test_case) # APPLY
+                result = analyzer.maximum_forks(test_input).get()
             elif test_method == 'most_forked_n':
-                pass
-                #self._most_forked_n_test(analyzer, test_case) # APPLY
+                result = analyzer.most_forked_n(test_input).get()
             elif test_method == 'with_substring':
-                pass
-                #self._with_substring_test(analyzer, test_case) # APPLY
+                result = analyzer.with_substring(test_input, test_case['field']).get()
             else:
                 assert False, ('''
                                 This else case should be never exevuted.
                                 There must a a fault about the naming of test method types.
                                 Check it.
                                 ''')
-   
+            # The result is type of pandas.DataFrame.
+            # We need to convert the result to set of tuples
+            # that represent GithubRepo objects.
+            list_of_named_tuples = list(result.itertuples(index=False))
+
+            # Convert the tuples to GithubRepos
+            set_of_repos = set()
+            for repo_info in list_of_named_tuples:
+                repo = GithubRepo(
+                        repo_info.owner,
+                        repo_info.repo_name,
+                        repo_info.description,
+                        repo_info.language,
+                        repo_info.stars,
+                        repo_info.forks
+                        )
+                set_of_repos.add(repo)
+            # Compare the result with expected result
+            assert set_of_repos == test_case['result']
+            
+            analyzer.clear()
+
     def _minimum_stars_test(self, analyzer, test_case):
         '''
         This function tests minimum_stars method in RepoAnalyzer class.
@@ -158,7 +174,6 @@ class TestRepoAnalyzerManager:
                     )
             set_of_repos.add(repo)
 
-        # Compare the result with expected result
+        # Codmpare the result with expected result
         assert set_of_repos == test_case['result']
-
 
